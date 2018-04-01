a228ce0ff4d97a4be25857cfbab94b34d1bdf758
Venkata Krishnan Anantha Raman <venkatakrishnansvpr@gmail.com>
2018-04-01 14:59:01 -0400

    more data grabs

---
+++ MapReduce/Scrapper.py
@@ -0,0 +1,4 @@
+import urllib2
+from bs4 import BeautifulSoup
+url = "https://www.nytimes.com/2018/03/19/technology/facebook-data-sharing.html"
+page = urllib2.urlopen(url)
--- NyTimes/datagrab_nytimes.py
+++ NyTimes/datagrab_nytimes.py
@@ -9,12 +9,11 @@ while (1):
     #dataurl = urllib2.urlopen('http://api.wunderground.com/api/e13fb09d4cd8343c/conditions/q/14214.json')
     key1 = 'f6394352039340ebb6d53343e41af8c3'
     key2 = '9995690d2daa49c098c69bdd24cd80ec'
-    url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key='+key2+'&q=facebook&begin_date=20180225&end_date=20180329&fl=lead_paragraph%2Cabstract%2Cheadline%2Csnippet&page='+str(count)
+    url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key='+key2+'&q=facebook&begin_date=20180225&end_date=20180329&fl=lead_paragraph%2Cabstract%2Cheadline%2Csnippet%2Cweb_url&page='+str(count)
     print (" Count : ",count," attempting :",url)
     dataurl = urllib2.urlopen(url)
     data_string = dataurl.read()
     data_json = json.loads(data_string)
-
     with open(str(count)+".json",'w') as out_json:
         json.dump(data_json, out_json)
         out_json.close()
