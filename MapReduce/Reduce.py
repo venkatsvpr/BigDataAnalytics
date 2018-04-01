95cbc10b42ece551c34655f772469668fe83db48
Venkata Krishnan Anantha Raman <venkatakrishnansvpr@gmail.com>
2018-04-01 19:52:19 -0400

    updates

--- MapReduce/Reduce.py
+++ MapReduce/Reduce.py
@@ -1,12 +1,14 @@
+#!/usr/bin/python
 import sys
-word = None;
-prev_word = None;
+word = None
+prev_word = None
 prev_count = 0;
-input = open("tmp/reduce_in.txt",'r')
-output = open("tmp/reduce_out.txt",'w')
-for line in input:
+for line in sys.stdin:
     line = line.strip()
-    word,count =  line.split('\t',1)
+    lt =  line.split('\t')
+    if (len(lt)!=2):
+        continue;
+    word,count = lt[0],lt[1]
     count = int(count)
     if (prev_word == None):
         prev_word = word
@@ -14,9 +16,7 @@ for line in input:
     elif (prev_word == word):
         prev_count += count
     else:
-        output.write(prev_word+"\t"+str(prev_count)+"\n")
+        print(prev_word+"\t"+str(prev_count))
         prev_word = word
         prev_count = count
-output.write(prev_word+"\t"+str(prev_count))
-output.close()
-input.close()
+print (prev_word+"\t"+str(prev_count))
--- MapReduce/Sort.py
+++ MapReduce/Sort.py
@@ -1,9 +1,1 @@
-import sys
-with open("tmp/map_out.txt",'r') as input:
-    lines = input.readlines()
-    sortedlines = sorted(lines)
-    with open("tmp/reduce_in.txt",'w') as output:
-        for line in sortedlines:
-            output.write(line)
-    output.close()
-input.close()
+not required
