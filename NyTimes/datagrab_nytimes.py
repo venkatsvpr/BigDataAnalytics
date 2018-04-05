0c28444d050e36f34aef4023736c95f2a93780a7
Venkata Krishnan Anantha Raman <venkatakrishnansvpr@gmail.com>
2018-04-05 16:21:41 -0400

    pushing the next set of data

--- NyTimes/datagrab_nytimes.py
+++ NyTimes/datagrab_nytimes.py
@@ -2,14 +2,14 @@ import csv
 import time
 import urllib2
 import json
-count = 59
+count = 0
 while (1):
     if (count > 900):
         break;
     #dataurl = urllib2.urlopen('http://api.wunderground.com/api/e13fb09d4cd8343c/conditions/q/14214.json')
     key1 = 'f6394352039340ebb6d53343e41af8c3'
     key2 = '9995690d2daa49c098c69bdd24cd80ec'
-    url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key='+key2+'&q=facebook&begin_date=20180329&end_date=20180402&fl=lead_paragraph%2Cabstract%2Cheadline%2Csnippet%2Cweb_url&page='+str(count)
+    url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key='+key2+'&q=facebook&begin_date=20170901&end_date=20171231&fl=lead_paragraph%2Cabstract%2Cheadline%2Csnippet%2Cweb_url&page='+str(count)
     print (" Count : ",count," attempting :",url)
     dataurl = urllib2.urlopen(url)
     data_string = dataurl.read()
@@ -23,7 +23,7 @@ while (1):
     if (breakflag):
         print ("brekflag set breaking")
         break;
-    with open("april_3/"+str(count)+".json",'w') as out_json:
+    with open("2017_sep1_dec31/"+str(count)+".json",'w') as out_json:
         json.dump(data_json, out_json)
         out_json.close()
     dataurl.close()
