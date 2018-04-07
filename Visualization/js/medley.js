dca204fc12711078b7ed2dfa8bb9ae2b24b6529d
Pratibha Barsale <pbarsale@buffalo.edu>
2018-04-07 19:02:35 -0400

    Part1 files added

--- Visualization/js/medley.js
+++ Visualization/js/medley.js
@@ -36,6 +36,7 @@ function load_data()
 
   var leaderscale = d3.scaleLinear().range([20,30]);
   var leaders = [];
+  var leaders1 = [];
   var fill = d3.scaleOrdinal(d3.schemeCategory10);
 
   d3.tsv("./data/" + file1, function(data){
@@ -63,6 +64,31 @@ function load_data()
         
   }); 
 
+  d3.tsv("./data/" + file2, function(data){
+
+    leaders1.push({text: data.Word, size: data.Freq});
+
+    leaderscale.domain([
+        d3.min(leaders1,function(d) {return d.size;}),
+        d3.max(leaders1,function(d) {return d.size;})        
+      ]);    
+
+    if(leaders1.length==show_data)
+    {      
+        layout = d3.layout.cloud()
+      .size([width, height])
+      .words(leaders1)
+      .padding(2)
+      .rotate(function() { return ~~(Math.random() * 2) * 90; })
+      .font("Impact")
+      .fontSize(function(d) { return leaderscale(d.size); })
+      .on("end", draw1);
+ 
+    layout.start();  
+    }
+        
+  }); 
+
   function draw(words) {
 
   d3.select("#word-cloud-NY").append("svg")
@@ -82,9 +108,25 @@ function load_data()
       })
       .text(function(d) { return d.text; });
   }
+
+  function draw1(words) {
+
+  d3.select("#word-cloud-TR").append("svg")
+      .attr("width", layout.size()[0])
+      .attr("height", layout.size()[1])
+    .append("g")
+      .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+    .selectAll("text")
+      .data(words)
+    .enter().append("text")
+      .style("font-size", function(d) { return d.size + "px"; })
+      .style("font-family", "Impact")
+      .style("fill", function(d, i) { return fill(i); })
+      .attr("text-anchor", "middle")
+      .attr("transform", function(d) {
+        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+      })
+      .text(function(d) { return d.text; });
+  }
+
 }
-
-
-
-
-
