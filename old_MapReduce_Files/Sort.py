4cf872c76b03b0049619dabd6e4717825e34c750
Venkata Krishnan Anantha Raman <venkatakrishnansvpr@gmail.com>
2018-04-08 03:08:38 -0400

    Folders Cleaned
    Cleaned Folders

--- old_MapReduce_Files/Sort.py
+++
@@ -1,9 +0,0 @@
-import sys
-with open("tmp/map_out.txt",'r') as input:
-    lines = input.readlines()
-    sortedlines = sorted(lines)
-    with open("tmp/reduce_in.txt",'w') as output:
-        for line in sortedlines:
-            output.write(line)
-    output.close()
-input.close()
